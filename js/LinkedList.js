585d4d36e91eafe1fcc53ae3a4abc6904f745f03
UdhayKrishn <krishnauday320@gmail.com>
2025-06-11 22:03:05 +0530

    feat: append method

---
+++ js/LinkedList.js
@@ -0,0 +1,61 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    increase() {
+        this.size++;
+    }
+
+    decrease() {
+        this.size--;
+    }
+
+    append(data) {
+        const node = new Node(data);
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+
+            let current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.increase();
+    }
+
+
+    display() {
+        let current = this.head;
+
+        while (current) {
+            console.log(current.data)
+            current = current.next;
+        }
+
+        console.log("size is: ", this.size)
+    }
+}
+
+
+const LL = new LinkedList();
+
+
+LL.append(10)
+LL.append(20)
+LL.append("H")
+LL.append(30)
+
+LL.display()
