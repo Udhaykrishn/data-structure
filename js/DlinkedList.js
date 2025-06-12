985c6d1e71f98b82b17f9bc756cd82654c958f43
UdhayKrishn <krishnauday320@gmail.com>
2025-06-12 17:04:11 +0530

    feat: delete method

--- js/DlinkedList.js
+++ js/DlinkedList.js
@@ -10,6 +10,7 @@ class Node{
 class DLinkedList{
     constructor(){
         this.head = null;
+        this.tail = null;
     }
 
     append(data){
@@ -17,6 +18,7 @@ class DLinkedList{
 
         if(!this.head){
             this.head = node;
+            this.tail = node;
             return;
         }
 
@@ -28,6 +30,7 @@ class DLinkedList{
 
         node.prev = current;
         current.next = node;
+        this.tail = node;
     }
 
 
@@ -35,6 +38,7 @@ class DLinkedList{
         const node = new Node(data)
         if(!this.head) {
             this.head = node;
+            this.tail = node;
             return 
         }
 
@@ -43,6 +47,40 @@ class DLinkedList{
         this.head = node
     }
 
+    delete(data){
+        if(!this.head) return null;
+
+
+        if(this.head.data === data){
+            if(this.head.next){
+                this.head.next.prev = null;
+            }
+            this.head = this.head.next;
+            return
+        }
+
+        if(this.tail.data === data){
+            this.tail.prev.next = null;
+            return;
+        }
+
+        let current = this.head
+
+
+        while(current && current.data !== data){
+            current = current.next;
+        }
+
+        if(!current) return null;
+
+        if(current.prev){
+            current.prev.next = current.next;
+        }
+
+        if(current.next){
+            current.next.prev = current.prev
+        }
+    }
 
     display(){
         if (!this.head) return;
@@ -64,8 +102,11 @@ const DLL = new DLinkedList()
 DLL.append(10)
 DLL.append(20)
 DLL.append(30)
-DLL.prepend(100)
-DLL.prepend(200)
+DLL.append(40)
+DLL.append(50)
+
+
+DLL.delete(50)
 
 DLL.display()
 
