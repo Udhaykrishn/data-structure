ce98057d479a86a954ca2740a2956bdc21d5854d
UdhayKrishn <krishnauday320@gmail.com>
2025-06-22 17:00:32 +0530

    feat: hash method

--- python/hash/sperate-chaining.hash.py
+++ python/hash/sperate-chaining.hash.py
@@ -3,3 +3,10 @@ class HashTable:
         self.size = size
         self.table = [[] for _ in range(size)]
         
+    
+    def hash(self,key):
+        hash_value = 0
+        
+        for i in range(len(key)):
+            hash_value += ord(key[i])
+        return hash_value % self.size
