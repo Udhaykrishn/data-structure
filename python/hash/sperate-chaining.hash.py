59fc0f939b6fea95aec50350f72eebe4a53e0b69
UdhayKrishn <krishnauday320@gmail.com>
2025-06-22 17:15:26 +0530

    feat: remove method

--- python/hash/sperate-chaining.hash.py
+++ python/hash/sperate-chaining.hash.py
@@ -27,8 +27,22 @@ class HashTable:
         bucket = self.table[index]
         
         
-        for i in range(len(key)):
+        for i in range(len(bucket)):
             if bucket[i][0] == key:
                 return bucket[i][1] 
             
         return None
+    
+    def remove(self,key):
+        index = self.hash(key)
+        bucket = self.table[index]
+        
+        for i in range(len(bucket)):
+            if bucket[i][0] == key:
+                bucket.pop(i)
+                break
+        
+        return None
+    
+    
+    
