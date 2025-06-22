f1c1b86da8fc2a58d16d9b5236eeb518f1e7dfee
UdhayKrishn <krishnauday320@gmail.com>
2025-06-22 17:02:47 +0530

    feat: set key value pair method

--- python/hash/sperate-chaining.hash.py
+++ python/hash/sperate-chaining.hash.py
@@ -10,3 +10,14 @@ class HashTable:
         for i in range(len(key)):
             hash_value += ord(key[i])
         return hash_value % self.size
+    
+    def set(self,key,value):
+        index = self.hash(key)
+        bucket = self.table[index]
+        
+        for i in range(len(bucket)):
+            if bucket[i][0] == key:
+                bucket[i][1] = value
+                break
+            
+        bucket.append([key,value])
