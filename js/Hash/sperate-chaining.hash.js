4c9fd7d9eafe7f02f5be76cce90111cf120f9450
UdhayKrishn <krishnauday320@gmail.com>
2025-06-22 16:30:26 +0530

    feat: hash method for find index

--- js/Hash/sperate-chaining.hash.js
+++ js/Hash/sperate-chaining.hash.js
@@ -9,4 +9,14 @@ class HashTable{
             this.tabe[i] = []
         }
     }
+
+    _hash(key){
+        let hash = 0;
+
+        for(let i = 0; i < key.length; i++){
+            hash += key.charCodeAt(i)
+        }
+
+        return hash % this.size
+    }
 }
