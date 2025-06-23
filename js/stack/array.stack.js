5d933e8b6a6c8f1e337dd2caf355a740a94d5385
UdhayKrishn <krishnauday320@gmail.com>
2025-06-23 16:11:56 +0530

    feat: added empty condition

--- js/stack/array.stack.js
+++ js/stack/array.stack.js
@@ -13,10 +13,14 @@ class Stack{
     }
 
     pop(){
+        if(this.empty())
+            return null;
         return  this.stack.pop()
     }
 
     peek(){
+        if(this.empty())
+            return null;
         return this.stack[this.stack.length - 1]
     }
 }
