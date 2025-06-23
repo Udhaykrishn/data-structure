e77a61686af45e7628d4b15a5b2249fd6bb06804
UdhayKrishn <krishnauday320@gmail.com>
2025-06-23 16:48:14 +0530

    feat: stack init

---
+++ python/stack/LL.stack.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+      
+    
+# stack = Stack()
+
+
+# stack.push(10)
+# stack.push(20)
+# stack.push(30)
+
+# stack.display()
+
+# print("\npeek: ",stack.peek(),"\n")
+
+stack.pop()
+
+stack.display()
