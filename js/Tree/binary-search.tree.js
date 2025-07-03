a0fc15e6ca7db8cd8409de164f01b7b3d0cf70fe
UdhayKrishn <krishnauday320@gmail.com>
2025-07-03 13:27:22 +0530

    feat: delete and min method

--- js/Tree/binary-search.tree.js
+++ js/Tree/binary-search.tree.js
@@ -41,6 +41,48 @@ class Tree{
         }
     }
 
+    delete(data){
+        return this._deleteNode(data,this.root)
+    }
+
+
+    _deleteNode(data,root){
+        if(!root){
+            return null;
+        }
+
+        if(data < root.data){
+            root.left = this._deleteNode(data,root.left);
+        }else if(data > root.data){
+            root.right = this._deleteNode(data,root.right);
+        }else{
+            if(!root.left && !root.right){
+                return null;
+            }
+
+            if(root.left){
+                return root.right;
+            }
+            if(root.right){
+                return root.left;
+            }
+
+
+            const min = this.min(root.right)
+            root.right = this._deleteNode(min,root.right)
+        }
+        return root;
+    }
+
+
+    min(root=this.root){
+        if(!root.left){
+            return root.data;
+        }else{
+            return this.min(root.left)
+        }
+    }
+
 
     bfs(){
         const queue = [];
@@ -71,5 +113,6 @@ tree.insert(15)
 tree.insert(25)
 tree.insert(8)
 
+tree.delete(8)
 
 tree.bfs() // 10 4 20 8 15 25
