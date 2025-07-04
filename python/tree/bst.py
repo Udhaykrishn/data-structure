7e6e9c8be478b2a46bce73e5e57ce92564fd9696
UdhayKrishn <krishnauday320@gmail.com>
2025-07-04 10:23:18 +0530

    feat: insert method

--- python/tree/bst.py
+++ python/tree/bst.py
@@ -7,3 +7,27 @@ class Node:
 class Tree:
     def __init__(self):
         self.root = None
+
+    
+    def insert(self,data,root):
+        node = Node(data)
+
+        if not self.root:
+            self.root = node
+            return
+    
+        current = self.root
+
+
+        while True:
+            if data < current.data:
+                if current.left == None:
+                    current.left = node
+                    return
+                current = current.left
+            else:
+                if current.right == None:
+                    current.right = node
+                    return
+                current = current.right
+        
